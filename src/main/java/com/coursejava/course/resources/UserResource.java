593fe1582366be7518c6a8bc0d3c6daa6a2674c3
Alysson Machado <alysson-am@hotmail.com>
2022-05-05 10:16:26 +1000

    DTO to return users

--- src/main/java/com/coursejava/course/resources/UserResource.java
+++ src/main/java/com/coursejava/course/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.coursejava.course.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coursejava.course.domain.User;
+import com.coursejava.course.dto.UserDTO;
 import com.coursejava.course.service.UserService;
 
 @RestController
@@ -21,11 +21,12 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = service.findAll();
+		List<UserDTO> listDTO  = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
