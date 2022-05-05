7c35c97be5fdc79c4c49c5e5fd0c6cb78c06241e
Alysson Machado <alysson-am@hotmail.com>
2022-05-06 09:47:27 +1000

    Creating entity Post with a nested User

--- src/main/java/com/coursejava/course/config/Instantiation.java
+++ src/main/java/com/coursejava/course/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.coursejava.course.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.coursejava.course.domain.Post;
 import com.coursejava.course.domain.User;
+import com.coursejava.course.repository.PostRepository;
 import com.coursejava.course.repository.UserRepository;
 
 @Configuration
@@ -15,17 +19,29 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 =  new Post(null, sdf.parse("21/03/2022"), "Holidays", "I am going to Ireland, Ihaaa", maria);
+		Post post2 =  new Post(null, sdf.parse("22/03/2022"), "Good morning", "I am so happy today", maria );
+		
+		
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
