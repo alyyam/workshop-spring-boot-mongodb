efab49a8390c3a2668b730c3612d6d470e81ec9e
Alysson Machado <alysson-am@hotmail.com>
2022-05-06 14:20:00 +1000

    Simple search usign query methods

--- src/main/java/com/coursejava/course/repository/PostRepository.java
+++ src/main/java/com/coursejava/course/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.coursejava.course.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,8 @@ import com.coursejava.course.domain.Post;
 @Repository
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
 
--- src/main/java/com/coursejava/course/service/PostService.java
+++ src/main/java/com/coursejava/course/service/PostService.java
@@ -1,12 +1,12 @@
 package com.coursejava.course.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.course.domain.Post;
-import com.coursejava.course.domain.User;
 import com.coursejava.course.repository.PostRepository;
 import com.coursejava.course.service.exception.ObjectNotFoundException;
 
@@ -22,6 +22,10 @@ public class PostService {
 	public Post findById(String id){
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
 
