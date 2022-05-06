55d3c2bdb2ee8782f9a08742f6dcbf088da80aca
Alysson Machado <alysson-am@hotmail.com>
2022-05-06 11:48:40 +1000

    Endpoint to return the users's post

--- src/main/java/com/coursejava/course/resources/UserResource.java
+++ src/main/java/com/coursejava/course/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.coursejava.course.domain.Post;
 import com.coursejava.course.domain.User;
 import com.coursejava.course.dto.UserDTO;
 import com.coursejava.course.service.UserService;
@@ -67,5 +68,13 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findByPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+
+	}
+
 
 }
