b437daed541fa5b0e398433c662a52fb440f167f
Alysson Machado <alysson-am@hotmail.com>
2022-05-06 13:28:10 +1000

    Adding comments to the post

--- src/main/java/com/coursejava/course/config/Instantiation.java
+++ src/main/java/com/coursejava/course/config/Instantiation.java
@@ -8,10 +8,12 @@ import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators.ArrayElemAt;
 
 import com.coursejava.course.domain.Post;
 import com.coursejava.course.domain.User;
 import com.coursejava.course.dto.AuthorDTO;
+import com.coursejava.course.dto.CommentDTO;
 import com.coursejava.course.repository.PostRepository;
 import com.coursejava.course.repository.UserRepository;
 
@@ -37,14 +39,18 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		
 		Post post1 =  new Post(null, sdf.parse("21/03/2022"), "Holidays", "I am going to Ireland, Ihaaa",new AuthorDTO(maria) );
 		Post post2 =  new Post(null, sdf.parse("22/03/2022"), "Good morning", "I am so happy today", new AuthorDTO(maria ));
 		
+		CommentDTO c1 =  new CommentDTO("Have a nice trip", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 =  new CommentDTO("Enjoy it", sdf.parse("23/03/2022"), new AuthorDTO(bob));
 		
-		
+		post1.getComments().addAll(Arrays.asList(c1));
+		post2.getComments().addAll(Arrays.asList(c2));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
